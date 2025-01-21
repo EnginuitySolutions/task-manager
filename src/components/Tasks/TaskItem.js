cd2f13603244b765acd8be5a0cd30962588bd4d7
Tabioka <donovanbrady102@gmail.com>
2025-01-21 02:29:40 -0500

    Update TaskItem.js

--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -1,1 +1,12 @@
 
+// src/components/Tasks/TaskItem.js
+import React from 'react';
+
+const TaskItem = ({ task, deleteTask }) => (
+    <li className="task-item">
+        <span>{task.text}</span>
+        <button onClick={() => deleteTask(task.id)}>Delete</button>
+    </li>
+);
+
+export default TaskItem;
